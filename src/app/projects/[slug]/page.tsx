0140330275609a81e38751d150140d5d2d6c3648
‘HarshitChejara’ <harshitchejara29@gmail.com>
2025-07-26 18:24:18 +0530

    changes

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,16 +1,21 @@
-// app/projects/[slug]/page.tsx
 import Contact from "app/components/Contact";
 import ProjectDetailSection from "app/components/ProjectDetailSection";
 import ServicesSection from "app/components/ServicesSection";
 
-export default function ProjectPage({ params }: { params: { slug: string } }) {
+type Props = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function ProjectPage({ params }: Props) {
   const { slug } = params;
 
   return (
     <main>
       <ProjectDetailSection slug={slug} />
       <ServicesSection />
-      <Contact/>
+      <Contact />
     </main>
   );
 }
