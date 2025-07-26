e914f6a5f87e18ba96ef7dd96fad5dfd3dd3cefc
‘HarshitChejara’ <harshitchejara29@gmail.com>
2025-07-26 18:41:06 +0530

    comment slug

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,21 +1,21 @@
-import Contact from "app/components/Contact";
-import ProjectDetailSection from "app/components/ProjectDetailSection";
-import ServicesSection from "app/components/ServicesSection";
+// import Contact from "app/components/Contact";
+// import ProjectDetailSection from "app/components/ProjectDetailSection";
+// import ServicesSection from "app/components/ServicesSection";
 
-type PageProps = {
-  params: {
-    slug: string;
-  };
-};
+// type PageProps = {
+//   params: {
+//     slug: string;
+//   };
+// };
 
-export default async function ProjectPage({ params }: PageProps) {
-  const { slug } = params;
+// export default async function ProjectPage({ params }: PageProps) {
+//   const { slug } = params;
 
-  return (
-    <main>
-      <ProjectDetailSection slug={slug} />
-      <ServicesSection />
-      <Contact />
-    </main>
-  );
-}
+//   return (
+//     <main>
+//       <ProjectDetailSection slug={slug} />
+//       <ServicesSection />
+//       <Contact />
+//     </main>
+//   );
+// }
