b7d772bb0ab48a7a9245fa7c3d36e78ba4b18284
‘HarshitChejara’ <harshitchejara29@gmail.com>
2025-08-07 19:31:56 +0530

    change favicon

---
+++ src/app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Perfect Creations</title>
+      <meta name="description" content="Your site description" />
+      <link rel="icon" href="/favicon.png" type="image/png"/>
+    </>
+  );
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,9 @@ import { FaWhatsapp } from "react-icons/fa";
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.png" />
+      </head>
       <body className="bg-[#0B0B14] text-white">
         <Header />
         {children}
